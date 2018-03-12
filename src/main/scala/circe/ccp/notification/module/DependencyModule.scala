bed5d04f6c7bea9127b07611f0a345575cba35fb
Phuong Lam <phuong@circe.xyz>
2018-03-12 18:16:10 +0700

    Update dependency

--- src/main/scala/circe/ccp/notification/module/DependencyModule.scala
+++ src/main/scala/circe/ccp/notification/module/DependencyModule.scala
@@ -1,10 +1,15 @@
 package circe.ccp.notification.module
 
+import circe.ccp.notification.service.{FakedNotificationService, NotificationService}
 import com.twitter.inject.TwitterModule
 
 /**
  * Created by phg on 3/12/18.
  **/
 object DependencyModule extends TwitterModule {
+  override def configure(): Unit = {
+    super.configure()
 
+    bind[NotificationService].to[FakedNotificationService]
+  }
 }
